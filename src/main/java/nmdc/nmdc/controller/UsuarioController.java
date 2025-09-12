d522a60b7aea16fbb53dbd7899092faedd83728c
ValeriaAc <valee5acatitla@gmail.com>
2025-09-12 15:40:59 -0600

    Se crean los fetch

--- src/main/java/nmdc/nmdc/controller/UsuarioController.java
+++ src/main/java/nmdc/nmdc/controller/UsuarioController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import nmdc.nmdc.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import nmdc.nmdc.model.Usuario;
 
 @RestController
 @RequestMapping(path="/api/usuario/")//http:localhost:8080/api/usuario/
+@CrossOrigin(origins = "http://127.0.0.1:5502")
 public class UsuarioController {
 	private final UsuarioService service;
 	@Autowired
--- src/main/java/nmdc/nmdc/controller/UsuarioRegistradoController.java
+++ src/main/java/nmdc/nmdc/controller/UsuarioRegistradoController.java
@@ -3,6 +3,7 @@ package nmdc.nmdc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import nmdc.nmdc.service.UsuarioRegistradoService;
 
 @RestController
 @RequestMapping (path="/api/usuarioRegistrado/")
+@CrossOrigin(origins = "http://127.0.0.1:5502")
 public class UsuarioRegistradoController {
 private final UsuarioRegistradoService service;
 @Autowired
